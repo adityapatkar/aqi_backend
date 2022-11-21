ced4cd8d6f1b70acfed5c217a61aaa29647840a7
Aditya Patkar <aditya.patkar@nonstopio.com>
2022-11-21 17:05:01 -0500

    Fixed url

--- cron.py
+++ cron.py
@@ -1,14 +1,14 @@
 import requests
 from env import city, state, url
 
-response = requests.get(url + "api", params={"city": city, "state": state})
+response = requests.get(url + f"aqi?city={city}&state={state})")
 if response.status_code == 200:
     print("Successfully fetched data")
     response = response.json()
     aqi = response["aqi"]
     city = response["city"]
     state = response["state"]
-    date_time = response["date_time"]
+    date_time = response["datetime"]
 
     #insert into database
     response = requests.post(url + "insert",
@@ -18,5 +18,7 @@ if response.status_code == 200:
                                  "aqi": aqi,
                                  "datetime": date_time
                              })
+    print(response.json())
 else:
     print("Error in fetching data")
+    print(response)
