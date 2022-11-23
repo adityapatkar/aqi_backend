2e8cde8824adc9257123e9c97cb65d344f05f156
Aditya Patkar <aditya.patkar@nonstopio.com>
2022-11-22 21:34:01 -0500

    Removed env variables

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-srv = "mongodb+srv://test:cJC3YjJvZoPwnF8K@msml.lphwikh.mongodb.net/?retryWrites=true&w=majority"
-url = "https://aqi-backend-msml.herokuapp.com/"
-city = "Mumbai"
-state = "Maharashtra"
+srv = os.environ['srv']
+url = os.environ['url']
+city = os.environ['city']
+state = os.environ['state']
